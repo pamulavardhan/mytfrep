424f134928e12dbaf0394d02b76e7350abca0a00
vardhanarao <pamula.rao@globallogic.com>
2022-09-16 15:30:47 +0530

    added terraform file for github-branch-protection

---
+++ create-github-branch-protection/create-github-branch-protection.tf
@@ -0,0 +1,27 @@
+provider "github" {
+    token = "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"
+
+}
+
+data "github_repository" "example" {
+  name        = "mytfrep"
+}
+
+# Protect the main branch of the foo repository. Only allow a specific user to merge to the branch.
+resource "github_branch_protection" "example" {
+  repository_id       = "${data.github_repository.example.name}"  
+  pattern             = "main"
+  enforce_admins      = true
+  
+
+  required_status_checks {
+    strict   = false
+    contexts = ["delete"]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews  = false
+    restrict_dismissals    = false
+  }
+
+}
